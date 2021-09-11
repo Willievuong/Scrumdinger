4129f3d84083f4977638dfefb52efde6e9a80bf4
willievuong <willievuong@gmail.com>
2021-09-11 16:20:03 -0700

    Completed 'Creating a CardView'

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -1,42 +1,34 @@
-//
-//  ContentView.swift
-//  Scrumdinger
-//
-//  Created by William Vuong on 9/11/21.
-//
-
 import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        VStack{
+        VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
                         .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
-
                 }
                 Spacer()
-                VStack {
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
                         .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
             .accessibilityElement(children: .ignore)
             .accessibilityLabel(Text("Time remaining"))
             .accessibilityValue(Text("10 minutes"))
             Circle()
-                .strokeBorder(lineWidth: 24, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-            HStack{
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
                 Text("Speaker 1 of 3")
                 Spacer()
-                Button(action:{}){
-                    Image( systemName: "forward.fill" )
+                Button(action: {}) {
+                    Image(systemName:"forward.fill")
                 }
-                .accessibilityLabel(Text("Next Speaker"))
+                .accessibilityLabel(Text("Next speaker"))
             }
         }
         .padding()
--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -1,9 +1,3 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by William Vuong on 9/11/21.
-//
 
 import SwiftUI
 
